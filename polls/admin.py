7b855cfa2828dc54803302a1f013842a48b8045f
root <root@localhost.localdomain>
2022-06-03 19:11:49 -0400

    comment test

--- polls/admin.py
+++ polls/admin.py
@@ -6,3 +6,15 @@ from .models import Choice
 
 admin.site.register(Question)
 admin.site.register(Choice)
+
+#test comment
+#test 2 comment
+#test 3 comment
+#test 4 comment
+#test 5 comment
+#test 6 comment
+#test 7 comment
+#test 8 comment
+#test 9 comment
+#test 10 comment
+#test 11 comment
