7cfb2f446c7a46d205be9a112eec5c9d2580cc0c
root <root@localhost.localdomain>
2022-06-05 13:32:35 -0400

    move test

---
+++ polls/and/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question
+from .models import Choice
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
+#test comment
+#test 2 comment
+#test 3 comment
+#test 4 comment
+#test 5 comment
+#test 6 comment
+#test 7 comment
+#test 8 comment
+#test 9 comment
+#test 10 comment
+#test 11 comment
