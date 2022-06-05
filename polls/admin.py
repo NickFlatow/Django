d65794818b4e4c28a59787d706a9dbaad0d9dd79
root <root@localhost.localdomain>
2022-06-05 19:20:42 -0400

    revert to 3c997e86df381ae9ac8ef52f071d765eda707650

--- polls/admin.py
+++ polls/admin.py
@@ -6,30 +6,8 @@ from .models import Choice
 
 admin.site.register(Question)
 admin.site.register(Choice)
-
 #add another class to admin.py:
 #from django.contrib import admin
 #from .models import Question
 #admin.site.register(Question)
 #add another class to admin.py:
-
-#test comment
-#test 2 comment
-#test 3 comment
-#test 4 comment
-#test 5 comment
-#test 6 comment
-#test 7 comment
-#test 8 comment
-#test 9 comment
-#test 10 comment
-#test 11 comment
-#test 12 comment
-#test 13 comment
-#test 14 comment
-#test 15 comment
-#test 16 comment
-#test 17 comment
-#test 18 comment
-#test 19 comment
-#test 20 comment
--- polls/tests.py
+++ polls/tests.py
@@ -1,14 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-#first new test
-#second new test
-#third new test
-#fourth new test
-#fifth new test
-#sixth new test
-#seventh new test
-#eighth new test
-#ninth new test
-#tenth new test
-
